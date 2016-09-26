5513bac6c2b9cda0b8c2ab782d747f22d3dcab64
sourishg <ragnarok0211@gmail.com>
2016-09-27 02:47:01 +0530

    Add fuzzification

--- plot/plot-membership.py
+++ plot/plot-membership.py
@@ -1,13 +1,25 @@
-import numpy as np
 import skfuzzy as fuzz
 import matplotlib.pyplot as plt
-import matplotlib.mlab as mlab
-import math
+import numpy as np 
 
-mu = 10
-variance = 1
-sigma = math.sqrt(variance)
-x = np.linspace(0, 24, 200)
-plt.plot(x,2.5*mlab.normpdf(x, mu, sigma))
+x = np.arange(0, 5, 0.01)
 
+mem1 = fuzz.trapmf(x, [0.45, 2, 3, 4.25])
+mem2 = fuzz.trapmf(x, [8, 10, 12, 14])
+mem3 = fuzz.trapmf(x, [13, 16, 24, 25])
+
+plt.figure(figsize=(10,8))
+ax = plt.subplot(111)
+
+ax.plot(x, mem1, 'b', linewidth=1.5, label='Fit')
+plt.xticks(np.arange(min(x), max(x)+1, 0.5))
+ax.legend()
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+ax.set_xlabel('Time')
+ax.set_ylabel('Membership')
+
+# Only show ticks on the left and bottom spines
+ax.yaxis.set_ticks_position('left')
+ax.xaxis.set_ticks_position('bottom')
 plt.show()
