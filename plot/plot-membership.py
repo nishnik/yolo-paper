c6744f724795efb4b25baab7b922110f7cda1972
sourishg <ragnarok0211@gmail.com>
2016-09-28 15:56:10 +0530

    Update paper

--- plot/plot-membership.py
+++ plot/plot-membership.py
@@ -4,15 +4,20 @@ import numpy as np
 
 x = np.arange(0, 5, 0.01)
 
-mem1 = fuzz.trapmf(x, [0.45, 2, 3, 4.25])
-mem2 = fuzz.trapmf(x, [8, 10, 12, 14])
-mem3 = fuzz.trapmf(x, [13, 16, 24, 25])
+mem1 = fuzz.trapmf(x, [-1, 0, 0.5, 1])
+mem2 = fuzz.trapmf(x, [0.75, 2, 3, 4.25])
+mem3 = fuzz.trapmf(x, [2.75, 4, 24, 25])
 
-plt.figure(figsize=(10,8))
+plt.figure(figsize=(10,5))
+plt.plot((0, 0.9), (0.2, 0.2), 'k--')
+plt.plot((0, 0.9), (0.12, 0.12), 'k--')
 ax = plt.subplot(111)
 
-ax.plot(x, mem1, 'b', linewidth=1.5, label='Fit')
-plt.xticks(np.arange(min(x), max(x)+1, 0.5))
+ax.plot(x, mem1, 'b', linewidth=1.5, label='Unfit')
+ax.plot(x, mem2, 'g', linewidth=1.5, label='Fit')
+ax.plot(x, mem3, 'r', linewidth=1.5, label='Proactive')
+ax.axvline(0.9, color='k', linestyle='-')
+plt.xticks(np.arange(min(x), max(x)+1, 1))
 ax.legend()
 ax.spines['right'].set_visible(False)
 ax.spines['top'].set_visible(False)
